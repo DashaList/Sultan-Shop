2c97500a1dcb11d8dfcc22ad0397faea480ab512
DashaList <dashalistopadova@mail.ru>
2023-04-05 23:00:47 +0700

    routes change

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Sultan-Shop/',
+  base: '/Sultan-Shop/', 
   plugins: [react(), svgr()],
 })
