3a09b7434244b0327c2116eb461bbb30017816ff
DashaList <dashalistopadova@mail.ru>
2023-04-06 01:02:54 +0700

    path

--- src/store/actions/adminActions.ts
+++ src/store/actions/adminActions.ts
@@ -6,7 +6,7 @@ import {adminSlice} from "../reducers/adminSlice";
 export const fetchAdminProducts = () => async (dispatch: AppDispatch) => {
     try {
         dispatch(adminSlice.actions.adminProductsFetching())
-        const response = await axios.get<ProductResponse>('https://dashalist.github.io/Sultan-Shop/products.json')
+        const response = await axios.get<ProductResponse>('https://dashalist.github.io/Sultan-Shop/main/db.json')
         dispatch(adminSlice.actions.adminProductsFetchingSuccess({
             products: response.data.results
         }))
