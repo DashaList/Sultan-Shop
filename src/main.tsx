d01b373f40d50fc9dac7be54e09bdf549d80af15
DashaList <dashalistopadova@mail.ru>
2023-04-06 00:18:53 +0700

    router change

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App/App'
 import './index.css'
 import { persistor, store } from './store/store'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import { PersistGate } from 'redux-persist/integration/react'
 
 
@@ -13,9 +13,9 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>,
